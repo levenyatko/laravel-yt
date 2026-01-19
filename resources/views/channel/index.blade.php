fd846cd3d7a756745a43311d88f861ff0714d594
levenyatko <levchenkodarja@gmail.com>
2026-01-19 17:26:29 +0100

    Moved channel edit link

--- resources/views/channel/index.blade.php
+++ resources/views/channel/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="bg-primary p-3 mb-5">
-    <div class="container">
+    <div class="container" style="position: relative;">
         <div class="d-flex align-items-center">
             @include('partials.channel-image')
             <div class="p-2">
@@ -12,22 +12,27 @@
                 <livewire:channel.subscribe :channel="$channel" style="light" />
             </div>
         </div>
+        @if (auth()->check() && $channel->isOwnedBy(auth()->user()))
+            <div class="dropdown" style="display: block; position: absolute; top: 0; right: -50px">
+                <a href="#" class="d-block btn bg-white rounded text-decoration-none" id="dropdownChannel" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="material-symbols-outlined">menu</i>
+                </a>
+                <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownChannel">
+                    <li>
+                        @can('update', $channel)
+                            <a class="dropdown-item" href="{{route('channel.edit', $channel)}}">Edit Channel</a>
+                        @endcan
+                    </li>
+                </ul>
+            </div>
+        @endif
     </div>
 </div>
 
 
 <div class="container">
-    <div class="d-flex justify-content-between align-items-center">
-        <div>
-            @can('update', $channel)
-            <a href="{{route('channel.edit', $channel)}}" class="btn btn-primary">Edit Channel</a>
-            @endcan
-        </div>
-    </div>
-
-    <div>
-        <div class="row my-4">
-            @foreach ($videos as $video)
+    <div class="row my-4">
+        @foreach ($videos as $video)
             <div class="col-12 col-md-6 col-lg-4">
                 <a href="{{ route('video.watch', $video)}}" class="card-link">
                     <div class="card mb-4" style="width: 333px; border:none;">
@@ -36,10 +41,7 @@
                 </a>
 
             </div>
-            @endforeach
-        </div>
-
+        @endforeach
     </div>
-
 </div>
 @endsection
